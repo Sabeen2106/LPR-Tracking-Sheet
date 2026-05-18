650019d2257a9e95f3081927436320eb5944b1a0
Sabeen2106 <sabeenfatima2106@gmail.com>
2026-05-18 11:37:09 +0100

    Refactor LPR app by adding business unit mapping

--- lpr_app.py
+++ lpr_app.py
@@ -16,7 +16,7 @@ business_unit_map = {
     "Larkshall": {"Sender Name": "Larkshall", "Sender Location Id": "C11254"},
     "Netherlands": {"Sender Name": "Netherlands", "Sender Location Id": "C6960"},
     "Spain": {"Sender Name": "Spain", "Sender Location Id": "C14430"},
-    "HQ": {"Sender Name": "HQ", "Sender Location Id": "C18571"}
+    "HQ": {"Sender Name": "HQ", "Sender Location Id": "C18571"},
     "Coca-Cola HBC Northern Ireland Ltd": {"Sender Name": "Coca-Cola HBC Northern Ireland Ltd", "Sender Location Id": "16928"}
 }
 
